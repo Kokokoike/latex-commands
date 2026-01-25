cbeef691069006eb7b4bd435126b5828e379473c
Kokokoike <yt.koike261715@gmail.com>
2026-01-25 23:48:10 +0900

    UIの修正

--- data/letters/data_letters.js
+++ data/letters/data_letters.js
@@ -9,7 +9,7 @@ setData({
             items: [
                 {
                     title: "ギリシャ文字",
-                    description: "ギリシャ文字の大文字・小文字と、異体字",
+                    description: "ギリシャ文字の大文字・小文字と、異体字の表示方法一覧です。アルファベットと同形でない場合、数式環境が必要です。",
                     file: "data/letters/greek/data_greek.js",
                     type: "leaf",
                     icon: "\\alpha"
